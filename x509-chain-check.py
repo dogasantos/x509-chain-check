4c107d10bf1616a8e2f6cd08ad2c696e1a58ddc4
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-12-04 10:52:45 -0200

    I have to change the method of reconstruct the chain of a certificate. This time, the authorityInfoAccess extension gives the URL for the CRT file used to sign the final entity digital certificate. All chain reconstruction is based on signature validation. There is a problem unfixed about four or more levels of AC in the chain. I could not find any x509 with more than 3 hops to the root AC where authorityInfoAccess extension is present on each level. Yes, sometimes authorityInfoAccess is not present so this script will fail.

--- x509-chain-check.py
+++ x509-chain-check.py
@@ -22,7 +22,9 @@
 #   Instalar m2crypto:
 #   bash$ pip install M2Crypto
 # 	bash$ pip install pyOpenSSL
-# 
+#
+#	Instalar pycrypto:
+# 	bash$ pip install pycrypto
 #
 # INSTRUÇÕES DE USO:
 # bash$ python t5.py <certificado> [container]
@@ -40,85 +42,140 @@
 import hashlib
 import M2Crypto as m2
 from OpenSSL import SSL, crypto
+from Crypto.Util import asn1
 import ssl
-import sys, os, re, datetime
+import sys, os, datetime
+import urllib2
+from stat import *
 
-class certificado(object):
-	def __init__(self):
-		self.len=0
-	data=None
-	data_hash=None
 
 
 revocation = None
 lista_certificados=[]
 status = False
+chain = False
 
+def ExtensoestoDict(cert):
+    certificateExtensions = {}
+    for index in range(cert.get_ext_count()):
+        ext = cert.get_ext_at(index)
+        certificateExtensions[ext.get_name()] = ext.get_value()
+    return certificateExtensions
 
-def BuscaCerts (string,container):
+def BuscaCerts(container):
 	achei = []
 	try:
 		dados = os.listdir(container)
-		for linha in dados: 
-			if re.match(string, linha):
-				achei.append(linha)	
+		for arquivo in dados: 
+			path=container+"/"+arquivo
+			if S_ISREG(os.stat(path)[ST_MODE]):
+				achei.append(arquivo)
 		return achei
 	except:
 		pass
 
+def GrabIssuerCrt(url):
+	#print "URL: "+url
+	buf = urllib2.urlopen(url).read()
+	try:
+		data = ssl.DER_cert_to_PEM_cert(buf)
+		x509 = crypto.load_certificate(crypto.FILETYPE_PEM, data)
+	except:
+		x509 = crypto.load_certificate(crypto.FILETYPE_PEM,buf)
+
+	x509ossl = crypto.dump_certificate(crypto.FILETYPE_PEM,x509)
+	x509m2 = m2.X509.load_cert_string(x509ossl)
+	return x509,x509ossl,x509m2
+
+def ValidateSign(cacert,efcert):
+	algoritmo=efcert.get_signature_algorithm()
+	certificado_asn1=crypto.dump_certificate(crypto.FILETYPE_ASN1, efcert)
+	der=asn1.DerSequence()
+	der.decode(certificado_asn1)
+
+	# der[0] - certificado
+	# der[1] - algoritmo
+	# der[2] - assinatura
+	sign=asn1.DerObject()
+	sign.decode(der[2])
+	sig0=sign.payload
+	# o primeiro byte é zero
+	# http://msdn.microsoft.com/en-us/library/windows/desktop/bb540792(v=vs.85).aspx
+	assinatura=sig0[1:]
+	try:
+		# certificado assinado pela ca fornecida
+		crypto.verify(cacert, assinatura, der[0], algoritmo)
+		return True 
+	except:
+		# certificado nao foi assinado por esta ca
+		return False
+
+def VerificarInfo(info):
+	v1=hash(frozenset(info.items()))
+	for mydict in reversed(lista_certificados):
+		v2=hash(frozenset(mydict.items()))
+		if v1==v2:
+			return True
+	return False
+
+def CertificadoEmissor(certificado_m2):
+	remotefile=None
+	extensoes_certificado=ExtensoestoDict(certificado_m2)
+	for k,v in extensoes_certificado.iteritems():
+		if k == "authorityInfoAccess":
+			componentes = v.split('\n')
+			for item in componentes:
+				if "CA Issuers" in item:
+					remotefile=item.replace('CA Issuers - URI:','')
+	return remotefile
+
 def RemontarChain(arquivo,container):
 	global status
-	d1={}
-	d2={}
-	
-	x509 = crypto.load_certificate(crypto.FILETYPE_PEM, file(arquivo).read())
-	emissor = x509.get_issuer().get_components()
-	x509pem = crypto.dump_certificate(crypto.FILETYPE_PEM,x509)
-	x509m2 = m2.X509.load_cert_string(x509pem)
-	d1["cert"]=x509m2
+	global chain
+	info={}
+	certificado = crypto.load_certificate(crypto.FILETYPE_PEM, file(arquivo).read())
+	certificado_pem = crypto.dump_certificate(crypto.FILETYPE_PEM,certificado)
+	certificado_m2 = m2.X509.load_cert_string(certificado_pem)
 
-	for k, v in dict(emissor).iteritems():
-		d1[k]=v
-	emissor_hash=hashlib.md5(x509.get_issuer().der()).hexdigest()
+	subject = certificado.get_subject().get_components()
 
-	d1["hashemissor"]=emissor_hash
-
-	subject = x509.get_subject().get_components()
 	for k, v in dict(subject).iteritems():
-		d2[k]=v	
+		info[k]=v
+	if VerificarInfo(info) == False:
+		lista_certificados.append(dict(info))
+	info={}
 
-	subject_hash=hashlib.md5(x509.get_subject().der()).hexdigest()
-	d2["isshash"]=subject_hash
-	
+	subject_hash=hashlib.md5(certificado.get_subject().der()).hexdigest() 
+	emissor_hash=hashlib.md5(certificado.get_issuer().der()).hexdigest()
+
+	# se nao for auto assinado:
 	if emissor_hash != subject_hash:
-		lista_certificados.append(d2)
-		lista_certificados.append(d1)
-		
-		cnemissor = dict(x509.get_issuer().get_components())['CN']
-		found = BuscaCerts((cnemissor.split(' ')[0] + '*'),container)
+		emissor_ptr=CertificadoEmissor(certificado_m2)
+		emissor_cert_openssl,emissor_cert_str,emissor_cert_m2=GrabIssuerCrt(emissor_ptr)
+		#res=ValidateSign(emissor_cert_openssl,certificado)
+		emissor = emissor_cert_openssl.get_subject().get_components()
+		for k, v in dict(emissor).iteritems():
+			info[k]=v
+		lista_certificados.append(dict(info))
+		info={}
 
+		found = BuscaCerts(container)
 		#explorar o container
 		for i in found:
-			f = file('{0}/{1}'.format(container, i), 'rb').read()
-			# PEGA A CHAVE PUBLICA DO CERTIFICADO
-			root_signature = (m2.X509.load_cert_string(f)).get_pubkey()
-			try:
-				# COMPARA AS CHAVES DOS CERTIFICADOS PARA DETECTAR SE O ROOT EH AUTENTICO
-				m2cert = m2.X509.load_cert_string(crypto.dump_certificate(crypto.FILETYPE_PEM, x509))
-				if m2cert.verify(root_signature) == 0:
-					# AS ASSINATURAS ESTAO BATENDO !!!
-					# agora faz a chamada recursiva para capturar informacoes do certificado raiz
-					cobj=container+"/"+i
-					RemontarChain(cobj,container)
-					status = True
-					break
-			except:
-				pass
-			#except Exception, j:
-			#	print j
+			#
+			# PARA MOSTRAR TODOS OS CERTIFICADOS ANALISADOS, DESCOMENTAR A LINHA ABAIXO:
+			#print "certificado: " +i
+			path = container + "/" + i 
+			certificado_root = crypto.load_certificate(crypto.FILETYPE_PEM, file(path).read())
+			res=ValidateSign(certificado_root,emissor_cert_openssl)
+			if res == True:
+				chain=True
+				RemontarChain(path,container)
 
 	else:
-		lista_certificados.append(d1)
+		if chain == True:
+			status = True
+
 	return True
 
 def VerChain():
@@ -129,28 +186,26 @@ def VerChain():
 		for k, v in item.iteritems():
 			if k != "cert":
 				print "%s %s: %s " %('\t'*loop,k,v)
-		for k, v in item.iteritems():
-			if k == "cert":
-				print "Certificado: "
-				print "%s %s" %('\t'*loop,v.as_pem())
 		print "%s-------------------------------------------------------" %('\t'*loop)
 
 
 
 def main():
-	if len(sys.argv) <2:
+	args=len(sys.argv)
+
+	if args <2:
 		print "%s <certificado do usuario> <diretorio contendo os certificados CA>" %sys.argv[0]
 		print "\t Por padrao, será utilizado o diretório /etc/ssl/certs/ do sistema, caso o usuario nao forneca o parametro"
 
-
-	try: 
+	if args == 3:
 		print "[*] Container: %s" %str(sys.argv[2])
 		RemontarChain(sys.argv[1],sys.argv[2])
-	except:
+	if args == 2:
 		print "[*] Container: /etc/ssl/certs/"
 		RemontarChain(sys.argv[1],'/etc/ssl/certs/')
+	
 	print "[*] A cadeia de certificados do arquivo selecionado:"
-	if not status:
+	if status == False:
 		print "  - Este certificado não é confiável, pois não possui emissor relacionado no container de certificados confiáveis"
 	else:
 		print "  - Este certificado é confiável pois possui emissor relacionado no container de certificados confiáveis"
